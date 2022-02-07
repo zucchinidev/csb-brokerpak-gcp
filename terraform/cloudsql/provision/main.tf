1cb8c94d15e114b8aa58c1b8aebca31c30bc3bb9
James Norman <normanja@vmware.com>
2022-02-07 08:34:21 +0000

    spike: explore adding attributes with no default value
    [#180992063](https://www.pivotaltracker.com/story/show/180992063)

--- terraform/cloudsql/provision/main.tf
+++ terraform/cloudsql/provision/main.tf
@@ -3,19 +3,44 @@ resource "google_sql_database_instance" "instance" {
   database_version = var.database_version
   region           = var.region
 
+
+  master_instance_name = var.master_instance_name
+  #replica_configuration = var.replica_configuration
+  root_password = var.root_password
+  #restore_backup_context = var.restore_backup_context
+  #clone = var.clone
+
   settings {
     tier        = local.service_tiers[var.cores]
     disk_size   = var.storage_gb
     user_labels = var.labels
+
+    activation_policy = var.activation_policy
+    availability_type = var.availability_type
+    collation = var.collation
+    disk_autoresize = var.disk_autoresize
+    disk_type = var.disk_type
+    pricing_plan = var.pricing_plan
 
     ip_configuration {
       ipv4_enabled    = false
       private_network = local.authorized_network_id
       #require_ssl = var.use_tls
     }
+
+    backup_configuration {
+        binary_log_enabled = var.binary_log_enabled
+        enabled = var.enabled
+        start_time = var.start_time
+        point_in_time_recovery_enabled = var.point_in_time_recovery_enabled
+        location = var.location
+        transaction_log_retention_days = var.transaction_log_retention_days
+    }
+
   }
 
-  deletion_protection = false
+
+  deletion_protection = var.deletion_protection
 }
 
 resource "google_sql_database" "database" {
--- terraform/cloudsql/provision/variables.tf
+++ terraform/cloudsql/provision/variables.tf
@@ -3,6 +3,28 @@ variable "authorized_network" { type = string }
 variable "authorized_network_id" { type = string }
 variable "instance_name" { type = string }
 variable "db_name" { type = string }
+variable "deletion_protection" { type = bool }
+variable "activation_policy" { type = string }
+variable "availability_type" { type = string }
+variable "collation" { type = string }
+variable "disk_autoresize" { type = bool }
+variable "disk_size" { type = number }
+variable "disk_type" { type = string }
+variable "pricing_plan" { type = string }
+
+variable "master_instance_name" { type = string }
+#variable "replica_configuration" { type = map(any) }
+variable "root_password" { type = string }
+#variable "restore_backup_context" { type = map(any) }
+#variable "clone" { type = map(any) }
+
+
+variable "binary_log_enabled" { type = bool }
+variable "enabled" { type = bool }
+variable "start_time" { type = string }
+variable "point_in_time_recovery_enabled" { type = bool }
+variable "location" { type = string }
+variable "transaction_log_retention_days" { type = number }
 
 variable "region" { type = string }
 variable "labels" { type = map(any) }
